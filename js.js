43f8ac16ccfd8c2ca6beecf8acb9e6f0ebbdee31
HappyRogelio7 <58347721+HappyRogelio7@users.noreply.github.com>
2021-01-13 05:14:12 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,17 @@
 
+// Discord.js 
+
+client.on("ready", () => {
+    console.log('mesage estoy listo o lista');   
+    setInterval(function() {
+      var estados = ["mensaje1","mensaje2"]
+      let estado = estados[Math.floor(estados.length * Math.random())];
+      client.user.setPresence({
+        status: "online", //dnd = no molestar, idle = Ausente , invible, online
+        activity: {
+          name: estado,
+          type: "WATCHING", //PLAYING = Jugando, WATCHING = Viendo, LISTENING = Escuchando, STREAMING = En Directo
+        }
+      })
+    }, 60000); 
+//tiempo en milisegundos. 60000 = 1 minuto```
